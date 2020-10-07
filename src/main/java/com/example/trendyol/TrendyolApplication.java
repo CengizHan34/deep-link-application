a43c34add127febee989edbe450e4eb46e024fbf
cengizhan <cengiz.han@hotmail.co.uk>
2020-10-07 11:31:32 +0300

    the application is dockerized

--- src/main/java/com/example/trendyol/TrendyolApplication.java
+++ src/main/java/com/example/trendyol/TrendyolApplication.java
@@ -2,15 +2,16 @@ package com.example.trendyol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@EnableElasticsearchRepositories
-@ComponentScan("com.example.trendyol")
 @SpringBootApplication
 public class TrendyolApplication {
 
     public static void main(String[] args) {
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         SpringApplication.run(TrendyolApplication.class, args);
     }
 
